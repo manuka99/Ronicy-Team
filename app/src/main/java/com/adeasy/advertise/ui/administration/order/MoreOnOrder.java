fbe45e997e8ba9c015800013d235151d509eddab
manukayasas94@gmail.com <manukad1>
2020-09-16 22:23:48 +0530

    manuka - admin ordersmore component update and confirm delivered compnt

--- app/src/main/java/com/adeasy/advertise/ui/administration/order/MoreOnOrder.java
+++ app/src/main/java/com/adeasy/advertise/ui/administration/order/MoreOnOrder.java
@@ -1,18 +1,26 @@
 package com.adeasy.advertise.ui.administration.order;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.app.DatePickerDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -22,6 +30,7 @@ import com.adeasy.advertise.manager.OrderManager;
 import com.adeasy.advertise.model.Order;
 import com.adeasy.advertise.util.CustomDialogs;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -35,6 +44,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class MoreOnOrder extends AppCompatActivity implements OrderCallback, AdapterView.OnItemSelectedListener, View.OnClickListener {
@@ -46,6 +56,7 @@ public class MoreOnOrder extends AppCompatActivity implements OrderCallback, Ada
     CustomDialogs customDialogs;
     Context context;
     TextInputEditText estimatedDateEditText;
+    FrameLayout snackbarLayout;
 
     //customer
     TextView name, address, email, phone;
@@ -72,6 +83,9 @@ public class MoreOnOrder extends AppCompatActivity implements OrderCallback, Ada
     boolean isHidden = false;
     String selectedEstimatedDate;
 
+    Button update, confirmDelivered;
+    ProgressBar progressBarDelivered, progressBarUpdate;
+
     private static final String TAG = "MoreOnOrder";
 
     @Override
@@ -94,7 +108,7 @@ public class MoreOnOrder extends AppCompatActivity implements OrderCallback, Ada
         hidden = findViewById(R.id.isHidden);
         placedDate = findViewById(R.id.orderPlacedDate);
         deliveredDateLayout = findViewById(R.id.layoutDelivered);
-        deliveredDate = findViewById(R.id.password);
+        deliveredDate = findViewById(R.id.orderDeliveredDate);
 
         //item
         itemID = findViewById(R.id.itemID);
@@ -109,6 +123,13 @@ public class MoreOnOrder extends AppCompatActivity implements OrderCallback, Ada
 
         orderDescription = findViewById(R.id.orderDescription);
         estimatedDate = findViewById(R.id.estimatedDate);
+
+        update = findViewById(R.id.update);
+        confirmDelivered = findViewById(R.id.confirmDelivered);
+        progressBarDelivered = findViewById(R.id.progressBarDelivered);
+        progressBarUpdate = findViewById(R.id.progressBarUpdate);
+
+        snackbarLayout = findViewById(R.id.snackbarLayout);
 
         order_status_list = new ArrayList<String>(Arrays.asList(getResources().getStringArray(R.array.order_status_array)));
         payment_status_list = new ArrayList<String>(Arrays.asList(getResources().getStringArray(R.array.payment_status_array)));
@@ -160,11 +181,39 @@ public class MoreOnOrder extends AppCompatActivity implements OrderCallback, Ada
         estimatedDate.getEditText().setOnClickListener(this);
         estimatedDateEditText.setOnClickListener(this);
         hidden.setOnClickListener(this);
+        update.setOnClickListener(this);
+        confirmDelivered.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.more_order_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemID = item.getItemId();
+        switch (itemID) {
+            case R.id.actionDelete:
+                return true;
+            case R.id.actionUser:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
     public void onCompleteInsertOrder(Task<Void> task) {
-
+        endUpdatingUi();
+        endDeliveryConfirmUI();
+        updateUiAfterSuccessfulConfirmDelivered();
+        if (task != null && task.isSuccessful())
+            showSuccessSnackbar("Order was updated successfully");
+        else if (task != null && task.getException() instanceof FirebaseFirestoreException && ((FirebaseFirestoreException) task.getException()).getCode().equals(FirebaseFirestoreException.Code.PERMISSION_DENIED))
+            customDialogs.showPermissionDeniedStorage();
+        else
+            showErrorSnackbar("Error: Order was not updated");
     }
 
     @Override
@@ -206,6 +255,7 @@ public class MoreOnOrder extends AppCompatActivity implements OrderCallback, Ada
             if (order.getDeliveredDate() != null) {
                 deliveredDateLayout.setVisibility(View.VISIBLE);
                 deliveredDate.setText(order.getDeliveredDate().toString());
+                updateUiAfterSuccessfulConfirmDelivered();
             } else {
                 deliveredDateLayout.setVisibility(View.GONE);
             }
@@ -254,6 +304,10 @@ public class MoreOnOrder extends AppCompatActivity implements OrderCallback, Ada
             onHideOrderClicked();
         else if (view == estimatedDate || view == estimatedDateEditText || view == estimatedDateEditText.getHint())
             showDatePicker();
+        else if (view == update)
+            updateOrder();
+        else if (view == confirmDelivered)
+            onConfirmDelivered();
     }
 
     private void onHideOrderClicked() {
@@ -280,6 +334,107 @@ public class MoreOnOrder extends AppCompatActivity implements OrderCallback, Ada
 
         }, newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
         StartTime.show();
+    }
+
+    private void updateOrder() {
+        order.setAvalability(isHidden);
+        order.setOrderDescription(orderDescription.getEditText().getText().toString());
+        order.setDeliveryEstimatedDate(estimatedDate.getEditText().getText().toString());
+        order.getPayment().setType(payment_status.getSelectedItem().toString());
+        order.setOrderStatus(order_status.getSelectedItem().toString());
+        orderManager.insertOrder(order, false);
+        showUpdatingUi();
+    }
+
+    private void onConfirmDelivered() {
+        if (order.getDeliveredDate() == null) {
+            new AlertDialog.Builder(this)
+
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+
+                    .setTitle("Are you sure you want to confirm order delivered?")
+
+                    .setMessage("Please note that any changes made cannot be reverted.")
+
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            showDeliveryConfirmUI();
+                            order.setDeliveredDate(new Date());
+                            orderManager.insertOrder(order, false);
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                        }
+                    })
+                    .show();
+        } else {
+            showErrorSnackbar("Order has been already delivered");
+            updateUiAfterSuccessfulConfirmDelivered();
+        }
+    }
+
+    private void updateUiAfterSuccessfulConfirmDelivered() {
+        if (order.getDeliveredDate() != null) {
+            confirmDelivered.setBackgroundColor(getResources().getColor(R.color.colorDangerLight));
+            confirmDelivered.setOnClickListener(null);
+            deliveredDate.setText(order.getDeliveredDate().toString());
+            deliveredDateLayout.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void showErrorSnackbar(String error) {
+        Snackbar snackbar = Snackbar.make(snackbarLayout, error, Snackbar.LENGTH_INDEFINITE).setAction("x", new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        }).setActionTextColor(getResources().getColor(R.color.colorWhite));
+        View snackbarView = snackbar.getView();
+        TextView textView = snackbarView.findViewById(com.google.android.material.R.id.snackbar_text);
+        textView.setMaxLines(5);
+        snackbar.getView().setBackgroundColor(getResources().getColor(R.color.colorError2));
+        snackbar.show();
+    }
+
+    private void showSuccessSnackbar(String message) {
+        Snackbar snackbar = Snackbar.make(snackbarLayout, message, Snackbar.LENGTH_INDEFINITE).setAction("x", new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        }).setActionTextColor(getResources().getColor(R.color.colorWhite));
+
+        snackbar.getView().setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+        snackbar.show();
+    }
+
+    private void showUpdatingUi() {
+        progressBarUpdate.setVisibility(View.VISIBLE);
+        update.setVisibility(View.GONE);
+    }
+
+    private void endUpdatingUi() {
+        progressBarUpdate.setVisibility(View.GONE);
+        update.setVisibility(View.VISIBLE);
+    }
+
+    private void showDeliveryConfirmUI() {
+        progressBarDelivered.setVisibility(View.VISIBLE);
+        confirmDelivered.setVisibility(View.GONE);
+    }
+
+    private void endDeliveryConfirmUI() {
+        progressBarDelivered.setVisibility(View.GONE);
+        confirmDelivered.setVisibility(View.VISIBLE);
+    }
+
+    private void showDeletingUI() {
+
+    }
+
+    private void endDeletingUI() {
+
     }
 
 }
