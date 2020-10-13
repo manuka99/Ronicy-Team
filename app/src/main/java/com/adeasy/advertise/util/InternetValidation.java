a56541d124ef53803a0df57804741af218a365bf
manukayasas94@gmail.com <manukad1>
2020-10-13 22:46:59 +0530

    manuka - functions updated

--- app/src/main/java/com/adeasy/advertise/util/InternetValidation.java
+++ app/src/main/java/com/adeasy/advertise/util/InternetValidation.java
@@ -4,6 +4,13 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by Manuka yasas,
  * University Sliit
@@ -13,18 +20,36 @@ import android.net.NetworkInfo;
 public class InternetValidation {
 
     public boolean validateInternet(Context context) {
-        boolean connected = false;
         try {
+            //check connected to a network
             ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
             if (connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
                     connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
-                //has internet
-                connected = true;
+                //connected to network
+
+                return true;
+
+                //check if it has internet
+//                InetAddress inetAddress = null;
+//                Future<InetAddress> future = Executors.newSingleThreadExecutor().submit(new Callable<InetAddress>() {
+//                    @Override
+//                    public InetAddress call() {
+//                        try {
+//                            return InetAddress.getByName("google.com");
+//                        } catch (UnknownHostException e) {
+//                            return null;
+//                        }
+//                    }
+//                });
+//                inetAddress = future.get(12, TimeUnit.SECONDS);
+//                future.cancel(true);
+//
+//                return inetAddress != null && !inetAddress.equals("");
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return connected;
+        return false;
     }
 
 }
