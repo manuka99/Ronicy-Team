9392b5e44dde92baeb4858eff227414b2640313c
manuka <manukayasas99@gmail.com>
2020-10-16 20:05:35 +0530

    manuka - orders fragment fixed

---
+++ app/src/main/java/com/adeasy/advertise/ui/advertisement/ReportAd.java
@@ -0,0 +1,16 @@
+package com.adeasy.advertise.ui.advertisement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.adeasy.advertise.R;
+
+public class ReportAd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.manuka_activity_report_ad);
+    }
+}
